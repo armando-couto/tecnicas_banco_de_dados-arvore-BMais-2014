6eb07ed7e7bbe1248039e1fa34fc8fcfc98d14b8
Armando Couto <armando-couto@hotmail.com>
2013-09-18 21:09:44 -0300

    Commit inicial.

---
+++ arvoreBMais/src/main/java/br/com/unifor/arvoreBMais/Registro.java
@@ -0,0 +1,23 @@
+package br.com.unifor.arvoreBMais;
+
+/**
+ * @author armandocouto
+ * @email coutoarmando@gmail.com
+ * @date 18/09/2013
+ */
+public class Registro {
+
+	private int numero;
+	
+	public Registro() {
+		numero = 0;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+}
